05d648ac01d018b9a31bd8c86f1e9c36c2c7bbfc
ysj_xdd <xdd@qq.com>
2018-01-10 13:22:37 +0800

    修改类名

--- src/main/java/com/java/xdd/spring/CustomRequestMappingHandlerMapping.java
+++ src/main/java/com/java/xdd/spring/CustomRequestMappingHandlerMapping.java
@@ -9,18 +9,18 @@ import java.lang.reflect.Method;
 public class CustomRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
 
     @Override
-    protected RequestCondition<ApiVesrsionCondition> getCustomTypeCondition(Class<?> handlerType) {
+    protected RequestCondition<ApiVersionCondition> getCustomTypeCondition(Class<?> handlerType) {
         ApiVersion apiVersion = AnnotationUtils.findAnnotation(handlerType, ApiVersion.class);
         return createCondition(apiVersion);
     }
 
     @Override
-    protected RequestCondition<ApiVesrsionCondition> getCustomMethodCondition(Method method) {
+    protected RequestCondition<ApiVersionCondition> getCustomMethodCondition(Method method) {
         ApiVersion apiVersion = AnnotationUtils.findAnnotation(method, ApiVersion.class);
         return createCondition(apiVersion);
     }
 
-    private RequestCondition<ApiVesrsionCondition> createCondition(ApiVersion apiVersion) {
-        return apiVersion == null ? null : new ApiVesrsionCondition(apiVersion.value());
+    private RequestCondition<ApiVersionCondition> createCondition(ApiVersion apiVersion) {
+        return apiVersion == null ? null : new ApiVersionCondition(apiVersion.value());
     }
 }
